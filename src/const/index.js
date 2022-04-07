8ba056066426576bebbf763f5904ad3ce2e22f70
@javascriptdev347 <javascriptdev347@gmail.com>
2022-04-07 09:17:33 +0500

    fake api teachers

--- src/const/index.js
+++ src/const/index.js
@@ -1,2 +1,3 @@
 // export const API_URL = "https://jsonplaceholder.typicode.com/";
 export const API_RANDOM = "https://randomuser.me/api/";
+export const FAKE_STORE_API = "https://fakestoreapi.com/";
